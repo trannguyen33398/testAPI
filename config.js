098855acbbbc6b65c47c51e9d405ad1a9f4c9e38
trannguyen33398 <trannguyen3398@gmail.com>
2020-08-29 01:54:33 +0700

    aaa

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
     "facebook_key"      :     "2355566978082007", //Điền App ID của bạn vào đây
     "facebook_secret"   :     "eddbcc4caaf2c9c2cf75bf3236a1db2e", //Điền App Secret ở đây
-    "callback_url"      :     "https://pacific-sierra-16703.herokuapp.com/auth/facebook/callback"
+    "callback_url"      :     "http://localhost:3006/auth/facebook/callback"
   }
