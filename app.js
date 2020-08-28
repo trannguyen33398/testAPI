f274e243c1133ef05fa1f977df5bcd47f2dc5769
trannguyen33398 <trannguyen3398@gmail.com>
2020-08-28 15:58:40 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const passport = require('passport');
+const FacebookStrategy  = require('passport-facebook').Strategy;
+const session  = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const routes = require('./routes');
+const app = express();
+
+// Passport session setup. 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Sử dụng FacebookStrategy cùng Passport.
+passport.use(new FacebookStrategy({
+    clientID: config.facebook_key,
+    clientSecret:config.facebook_secret ,
+    callbackURL: config.callback_url
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      console.log(accessToken, refreshToken, profile, done);
+      return done(null, profile);
+    });
+  }
+));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs'); // sử dụng view ejs
+app.use(cookieParser()); //Parse cookie
+app.use(bodyParser.urlencoded({ extended: false })); //Parse body để get data
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));  //Save user login
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', routes);
+
+app.listen(3000);
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports={
+    "facebook_key"      :     "2355566978082007", //Điền App ID của bạn vào đây
+    "facebook_secret"   :     "eddbcc4caaf2c9c2cf75bf3236a1db2e", //Điền App Secret ở đây
+    "callback_url"      :     "http://localhost:3000/auth/facebook/callback"
+  }
